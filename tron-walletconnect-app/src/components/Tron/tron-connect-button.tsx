ac57120df5ab14a7ebee0d08798ca86f974f56d9
Vladyslav Kurko <v.kurko2012@gmail.com>
2024-11-24 13:59:12 +0100

    fix build

--- tron-walletconnect-app/src/components/Tron/tron-connect-button.tsx
+++ tron-walletconnect-app/src/components/Tron/tron-connect-button.tsx
@@ -1,41 +1,16 @@
 import { useWallet } from "@tronweb3/tronwallet-adapter-react-hooks";
-import { WalletDisconnectButton, WalletConnectButton } from "@tronweb3/tronwallet-adapter-react-ui";
 import TronLinkIcon from "../../assets/tron-trx-icon.png";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import '@tronweb3/tronwallet-adapter-react-ui/style.css';
 // import { WalletConnectButton } from "@tronweb3/tronwallet-adapter-react-ui";
 // import { WalletActionButton } from "@tronweb3/tronwallet-adapter-react-ui";
 import "./tron-connect-button.css";
-import { tronWeb } from "../../tronweb";
 
 export default function TronConnectButton() {
     // const { select } = useWallet();
     const { disconnect, connect, connected, select } = useWallet();
 
     select('TronLink' as any);
-
-    const handleDisconnect = async () => {
-      if (connected) {
-        try {
-          // Disconnect from the wallet adapter
-          await disconnect();
-    
-          // Clear any TronLink session
-          if (tronWeb && tronWeb.defaultAddress.base58) {
-            tronWeb.clearAddress(); // Logout from TronLink
-          }
-
-           // Clear localStorage or cookies if applicable
-          localStorage.clear();
-          document.cookie = "";
-    
-          // Optionally reload the page
-          // window.location.reload();
-        } catch (error) {
-          console.error("Failed to disconnect:", error);
-        }
-      }
-    };
 
     return (
       // <WalletActionButton></WalletActionButton>
